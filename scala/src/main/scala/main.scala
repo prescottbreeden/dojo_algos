1cf9e00d53e5f97cf29948f188945a8ef8ef4872
prescottbreeden <prescott.breeden@gmail.com>
2019-07-04 12:52:02 -0700

    updating repo

--- scala/src/main/scala/main.scala
+++ scala/src/main/scala/main.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 object Main extends App {
 
   def sigma(num: Int): Int = {
@@ -5,8 +6,27 @@ object Main extends App {
     else num + sigma(num-1)
   }
 
+  def fibonacci(num: Int): Int = {
+    if (num <= 1) num
+    else fibonacci(num - 1) + fibonacci(num - 2)
+  }
+
+  def fibTail(n: Long): Long = {
+    if (n <= 1) n
+
+    @tailrec
+    def fibHelper(a: Long, b: Long, n: Long): Long = {
+      if (n == 0) b
+      else fibHelper(b, a + b, n-1)
+    }
+    fibHelper(1, 1, n-2)
+  }
+
+
   def isPalindrome(s: String) = {
     val test: String = s.toLowerCase()
+
+    @tailrec
     def palHelper(test: String, start: Int, end: Int): Boolean = {
       if ((end - start) <= 1) true
       else if (test.charAt(start) != test.charAt(end)) false
@@ -15,9 +35,14 @@ object Main extends App {
     palHelper(test, 0, test.length-1)
   }
 
-  def fibonacci(num: Int): Int = {
-    if (num <= 1) num
-    else fibonacci(num - 1) + fibonacci(num - 2)
+  def isPrime(n: Int): Boolean = {
+
+    @tailrec
+    def isPrimeUntil(t: Int): Boolean = {
+      if (t <= 1) true
+      else n % t != 0 && isPrimeUntil(t-1)
+    }
+    isPrimeUntil(n / 2)
   }
 
 
@@ -28,6 +53,9 @@ object Main extends App {
   println("============================")
   println(sigma(19))
   println(isPalindrome("bobba"))
-  println(fibonacci(5))
+  println(fibonacci(12))
+  println(fibTail(50))
+  println(isPrime(11))
+  println(isPrime(11 * 4))
 
 }
